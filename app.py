cca0cae70a7b5c69a8be3aba6b84fd9b46e11ac8
Waldemberg <waldemberg.pereira@outlook.com>
2026-09-25 22:47:25 -0300

    feat: adicionar ci e instalar pytest

--- app.py
+++ app.py
@@ -1,14 +1,15 @@
 import streamlit as st
 from pathlib import Path
 
-
-st.set_page_config(page_title="Data manager", page_icon=":material/edit:", layout='wide')
-
 BASE_DIR = Path(__file__).parent
 
+# Especificação da página
+st.set_page_config(page_title="Data manager", page_icon=":material/edit:", layout='wide')
+
+# Páginas
 dashboard_page = st.Page( BASE_DIR / "pages/dashboard.py", title="Dashboard")
 home_page = st.Page(BASE_DIR / "pages/home.py", title="Início")
 
 pg = st.navigation([home_page, dashboard_page])
 
-pg.run()    
+pg.run()
